4fd17902d1a2ce49d8fb6b47ab9f10705c6e1d06
SilenceJT <jessewjt@gmail.com>
2021-04-16 17:19:25 +1000

    modify config

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-config.assets.initialize_on_precompile = false
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -32,5 +32,6 @@ module PmMongodb
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile = false
   end
 end
