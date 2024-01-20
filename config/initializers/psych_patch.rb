31d0c8043bc7b1b62c00dc5e9051329dd09b39b8
SilenceJT <jessewjt@gmail.com>
2024-01-20 18:44:25 +1100

    fix: Add back config

--- config/initializers/psych_patch.rb
+++ config/initializers/psych_patch.rb
@@ -1,12 +1,12 @@
 module Psych
-  # def self.load yaml, permitted_classes: [Symbol], permitted_symbols: [], aliases: true, filename: nil, fallback: nil, symbolize_names: false, freeze: false, strict_integer: false
-  #   safe_load yaml, permitted_classes: permitted_classes,
-  #                   permitted_symbols: permitted_symbols,
-  #                   aliases: aliases,
-  #                   filename: filename,
-  #                   fallback: fallback,
-  #                   symbolize_names: symbolize_names,
-  #                   freeze: freeze,
-  #                   strict_integer: strict_integer
-  # end
+  def self.load yaml, permitted_classes: [Symbol], permitted_symbols: [], aliases: true, filename: nil, fallback: nil, symbolize_names: false, freeze: false, strict_integer: false
+    safe_load yaml, permitted_classes: permitted_classes,
+                    permitted_symbols: permitted_symbols,
+                    aliases: aliases,
+                    filename: filename,
+                    fallback: fallback,
+                    symbolize_names: symbolize_names,
+                    freeze: freeze,
+                    strict_integer: strict_integer
+  end
 end
